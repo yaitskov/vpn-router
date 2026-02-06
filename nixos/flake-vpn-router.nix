d8fe08001318ea56bb0020dc396554216b352360
Daniil Iaitskov <dyaitskov@gmail.com>
2026-02-06 19:35:47 +0300

    master after cups

--- nixos/flake-vpn-router.nix
+++ nixos/flake-vpn-router.nix
@@ -76,7 +76,7 @@ in {
     };
     systemd.services.vpn-router = {
       wantedBy = [ "multi-user.target" ];
-      after = [ "network-setup.service" ];
+      after = [ "cups.service" ];
       enable = true;
       serviceConfig = {
         User = "vpn-router";
