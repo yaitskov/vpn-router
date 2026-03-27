dec1cca0366ae62791c1a001943aadd5ed1fced1
Daniil Iaitskov <dyaitskov@gmail.com>
2026-03-27 22:06:29 +0300

    TypeCss prototype

---
+++ ui/TypedCss.hs
@@ -0,0 +1,119 @@
+module TypedCss (CssClass, css, class_) where
+
+import Data.CSS.Syntax.Tokens ( tokenize, Token(Ident, Delim) )
+import Data.Char
+    ( isAlpha, isAlphaNum, isUpper, toLower, toUpper )
+import Data.Set ( insert, Set, toList )
+import Data.Text ( Text, pack, unpack )
+import Language.Haskell.TH.Quote ( QuasiQuoter(..) )
+import Language.Haskell.TH.Syntax
+    ( mkName,
+      Exp(LitE, AppE, ConE),
+      Clause(Clause),
+      Type(ConT, AppT),
+      Dec(FunD, PragmaD, SigD),
+      Phases(AllPhases),
+      Body(NormalB),
+      Inline(Inline),
+      Lit(StringL),
+      Pragma(InlineP),
+      RuleMatch(FunLike) )
+import Prelude
+
+newtype CssClass s = CssClass { unCssClass :: s } deriving (Show, Eq, Ord)
+
+instance Semigroup (CssClass Text) where
+  CssClass l <> CssClass r = CssClass $ l <> " " <> r
+
+class_ ::  CssClass Text -> Text
+class_ = unCssClass
+
+css :: QuasiQuoter
+css = QuasiQuoter
+  { quoteExp  = \_ -> fail "quoteExp: not implemented"
+  , quotePat  = \_ -> fail "quotePat: not implemented"
+  , quoteType = \_ -> fail "quoteType: not implemented"
+  , quoteDec  = pure . cssToDecs
+  }
+
+{-
+Delim '.',Ident "skeleton-block",Colon,Function "not",Colon,Ident "last-child",RightParen
+-}
+collectReferedClasses :: Set (CssClass Text) -> [Token] -> Set (CssClass Text)
+collectReferedClasses s = \case
+  Delim '.' : Ident cn : t ->
+    collectReferedClasses (insert (CssClass cn) s) t
+  _ : t -> collectReferedClasses s t
+  [] -> s
+
+{- | generate definition like:
+
+@@
+  {-# INLINE foo #-}
+  foo :: IsString s => CssClass s
+  foo = "foo"
+@@
+
+-}
+cssClassConstDec :: CssClass Text -> [Dec]
+cssClassConstDec (CssClass cn) =
+  [ PragmaD (InlineP n Inline FunLike AllPhases)
+  , SigD n (AppT (ConT ''CssClass) (ConT ''Text))
+  , FunD n [ Clause [] body [] ]
+  ]
+  where
+    ns = unpack cn
+    n = mkName $ escapeIden ns
+    body = NormalB (AppE (ConE 'CssClass) (LitE (StringL ns)))
+
+cssToDecs :: String -> [Dec]
+cssToDecs s = go $ pack s
+  where
+    go = (cssAsLiteralTextD s <>) . concatMap cssClassConstDec . toList . collectReferedClasses mempty . tokenize
+
+{- | generate definition like:
+@@
+  {-# INLINE cssAsLiteralText #-}
+  cssAsLiteralText :: Text
+  cssAsLiteralText = s
+@@
+-}
+cssAsLiteralTextD :: String -> [Dec]
+cssAsLiteralTextD s =
+  [ SigD n (ConT ''Text)
+  , FunD n [ Clause [] body [] ]
+  , PragmaD (InlineP n Inline FunLike AllPhases)
+  ]
+  where
+    n = mkName "cssAsLiteralText"
+    body = NormalB (LitE (StringL s))
+
+escapeIden :: String -> String
+escapeIden s =
+  case escapeIdenChar <$> hyphensToCamelCase s of
+    s'@(fl:ol)
+      | not (isAlpha fl) -> '_' : s'
+      | isUpper fl -> toLower fl : ol
+      | otherwise -> s'
+    [] -> []
+
+escapeIdenChar :: Char -> Char
+escapeIdenChar c
+  | isAlphaNum c || c == '_' = c
+  | otherwise = '_'
+
+hyphensToCamelCase :: String -> String
+hyphensToCamelCase = concatMap ucFirst . splitOn '-'
+
+ucFirst :: String -> String
+ucFirst "" = ""
+ucFirst (h:t) = toUpper h : t
+
+splitOn :: Eq a => a -> [a] -> [[a]]
+splitOn x xs = go xs []
+  where
+    go [] acc = [reverse acc]
+    go (y : ys) acc =
+      if x == y
+      then reverse acc : go ys []
+      else go ys (y : acc)
--- ui/Ui.hs
+++ ui/Ui.hs
@@ -4,17 +4,19 @@
 {-# LANGUAGE QuasiQuotes #-}
 
 module Main where
+import CssStyle
 import Data.Maybe ( maybeToList )
+import Data.Text
 import Miso
 import Miso.FFI.QQ (js)
 import Miso.Html.Element (div_, button_)
 import Miso.Html.Element qualified as H
 import Miso.Html.Event qualified as E
 import Miso.Html.Event (onClick)
-import Miso.Html.Property (class_)
 import Miso.Html.Property qualified as P
 import Miso.Lens ( Lens, lens, (?=), (^.) )
 import Prelude
+import TypedCss qualified as TC
 
 #ifdef WASM
 foreign export javascript "hs_start" main :: IO ()
@@ -59,70 +61,7 @@ app = (component emptyModel updateModel viewModel)
         });
       """
     ]
-  , styles =
-    [ Style """
-        .github-link {
-            position: fixed;
-            padding: 4vh;
-            right: 0vh;
-        }
-        .github-link img {
-            width: 5vh;
-            opacity: 0.4;
-        }
-
-        .restart-vpn {
-            position: fixed;
-            padding: 3vh;
-        }
-        .restart-vpn button {
-            font-size: xxx-large;
-            padding: 1vh;
-            border-width: thin;
-            background: transparent;
-            color: #7a83d1;
-            border-color: #7a83d1;
-        }
-        body { overflow: hidden; }
-        .butdiv {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          height: 100vh;
-          background: radial-gradient(circle, rgba(34, 193, 195, 1) 0%, rgba(253, 187, 45, 1) 100%);
-        }
-        button {
-          font-weight: bold;
-          font-size: xxx-large;
-          border-radius: 4vh;
-          padding: 2vh 3vh;
-          border: 8px black solid;
-        }
-        button:focus {
-          outline-offset: 2vh;
-        }
-        button.red {
-          color: #fc2c2c;
-          border-color: #fc2c2c;
-          background: linear-gradient(33deg, rgb(124 133 167) 0%, rgb(182 182 236) 12%, rgb(136 246 143) 99%);
-        }
-        button.green {
-          color: green;
-          border-color: green;
-          background: linear-gradient(33deg, rgb(124 133 167) 0%, rgb(182 182 236) 12%, rgb(136 246 143) 99%);
-        }
-        .ipaddr {
-          display: block;
-          position: fixed;
-          right: 4vh;
-          bottom: 3vh;
-          opacity: 0.5;
-          font-size: xxx-large;
-          background: transparent;
-        }
-
-      """
-    ]
+  , styles = [ Style $ ms CssStyle.cssAsLiteralText ]
   }
 
 emptyModel :: Model
@@ -166,17 +105,20 @@ updateModel = \case
   ErrorHandler Response {..} ->
     io_ (consoleError body)
 
+class_ :: TC.CssClass Text -> Attribute action
+class_ = P.class_ . ms . TC.class_
+
 viewModel :: Model -> View Model Action
 viewModel m = div_ [] $ [ header ] <> pages
   where
     header =
-      H.div_ [ P.class_ "github-link" ]
+      H.div_ [ class_ githubLink ]
         [ H.a_ [ P.href_ "https://github.com/yaitskov/vpn-router"
                , P.alt_  "Link to VpnRouter project"
                ]
                [ H.img_ [ P.src_ "/github.svg" ] ]
         ]
-    clientIpDiv = div_ [ class_ "ipaddr" ] $ maybeToList (text <$> clientIp m)
+    clientIpDiv = div_ [ class_ ipaddr ] $ maybeToList (text <$> clientIp m)
     pages =
       if restartConfirmationDialog m then
         restart
@@ -185,33 +127,35 @@ viewModel m = div_ [] $ [ header ] <> pages
     restart =
       [ clientIpDiv
       , div_
-          [ class_ "butdiv" ]
+          [ class_ butdiv ]
           [ button_
-              [ class_ "red", onClick ConfirmVpnRestart ]
+              [ class_ red, onClick ConfirmVpnRestart ]
               [ "RESTART VPN" ]
           ]
       ]
     home =
       [ clientIpDiv
        , div_
-         [ class_ "restart-vpn" ]
+         [ class_ restartVpn ]
          [ button_
            [ P.title_ "restart VPN", onClick AskForRestart ]
            [ "↻" ]
          ]
       , div_
-          [ class_ "butdiv" ]
+          [ class_ butdiv ]
           [ case m ^. info of
               Nothing -> text "Loading ..."
               Just VpnBypassOn ->
                 button_
-                [ class_ "green autofocus"
+                [ class_ green
+                , P.autofocus_ True
                 , E.onClick ToggleVpnStatus
                 ]
                 [ "Use VPN" ]
               Just VpnBypassOff ->
                 button_
-                [ class_ "red autofocus"
+                [ class_ red
+                , P.autofocus_ True
                 , E.onClick ToggleVpnStatus ]
                 [ "Bypass VPN" ]
           ]
